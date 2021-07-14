96612e9d2c0d2f7deaac03ab1e8c34a4a2d377fe
walleser-karli <k.walleser@ufl.edu>
2021-07-13 22:22:21 -0400

    Fill in first three temps with arrow functions

--- src/main.js
+++ src/main.js
@@ -1,1 +1,16 @@
-// It is always helpful to use comments in your code!
+// The number “212” immediately prints inside of the span with the ID "boiling-temp" using an arrow function
+const boilingTemp = boil => document.getElementById("boiling-temp").innerHTML = 212;
+
+boilingTemp(212);
+
+// The number “32” immediately prints inside of the span with the ID "freezing-temp" using an arrow function
+
+const freezingTemp = freeze => document.getElementById("freezing-temp").innerHTML = "32";
+
+freezingTemp("32");
+
+// Any number of your choice immediately prints inside of the span with the ID "water-temp" using an arrow function
+const waterTemp = water => document.getElementById("water-temp").innerHTML = "66";
+
+waterTemp("66");
+
